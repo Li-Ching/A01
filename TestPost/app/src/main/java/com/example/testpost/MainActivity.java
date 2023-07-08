b82c0a2e7b59ce5b7aafe5c5a6b86b7a481ae5fa
Li-Ching <c13575@gmail.com>
2023-07-08 14:57:18 +0800

    測試POST方法

---
+++ TestPost/app/src/main/java/com/example/testpost/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.testpost;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    // creating variables for our edittext,
+    // button, textview and progressbar.
+    private EditText usernameEdt, emailEdt, passwordEdt;
+    private Button postDataBtn;
+    private TextView responseTV;
+    private ProgressBar loadingPB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initializing our views
+        usernameEdt = findViewById(R.id.idEdtName);
+        emailEdt = findViewById(R.id.idEdtEmail);
+        passwordEdt = findViewById(R.id.idEdtPasswd);
+        postDataBtn = findViewById(R.id.idBtnPost);
+        responseTV = findViewById(R.id.idTVResponse);
+        loadingPB = findViewById(R.id.idLoadingPB);
+
+        // adding on click listener to our button.
+        postDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // validating if the text field is empty or not.
+                if (usernameEdt.getText().toString().isEmpty() && emailEdt.getText().toString().isEmpty() && passwordEdt.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please fill in the empty spaces", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // calling a method to post the data and passing our name and job.
+                postData(usernameEdt.getText().toString(), emailEdt.getText().toString(), passwordEdt.getText().toString());
+            }
+        });
+    }
+
+    private void postData(String name, String email, String password) {
+
+        // below line is for displaying our progress bar.
+        loadingPB.setVisibility(View.VISIBLE);
+
+        // on below line we are creating a retrofit
+        // builder and passing our base url
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://140.137.41.136:1380/A01/")
+                // as we are sending data in json format so
+                // we have to add Gson converter factory
+                .addConverterFactory(GsonConverterFactory.create())
+                // at last we are building our retrofit builder.
+                .build();
+        // below line is to create an instance for our retrofit api class.
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
+
+        // passing data from our text fields to our modal class.
+        DataModal modal = new DataModal(name, email, password);
+
+        // calling a method to create a post and passing our modal class.
+        Call<DataModal> call = retrofitAPI.createPost(modal);
+
+        // on below line we are executing our method.
+        call.enqueue(new Callback<DataModal>() {
+            @Override
+            public void onResponse(Call<DataModal> call, Response<DataModal> response) {
+                // this method is called when we get response from our api.
+                Toast.makeText(MainActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
+
+                // below line is for hiding our progress bar.
+                loadingPB.setVisibility(View.GONE);
+
+                // on below line we are setting empty text
+                // to our both edit text.
+                usernameEdt.setText("");
+                emailEdt.setText("");
+                passwordEdt.setText("");
+
+                // we are getting response from our body
+                // and passing it to our modal class.
+                DataModal responseFromAPI = response.body();
+
+                // on below line we are getting our data from modal class and adding it to our string.
+                String responseString = "Response Code : " + response.code() + "\nName : " + responseFromAPI.getUsername() + "\n" + "Email : " + responseFromAPI.getEmail() + "\n" + "Password : " + responseFromAPI.getPassword();
+
+                // below line we are setting our
+                // string to our text view.
+                responseTV.setText(responseString);
+            }
+
+            @Override
+            public void onFailure(Call<DataModal> call, Throwable t) {
+                // setting text to our text view when
+                // we get error response from API.
+                responseTV.setText("Error found is : " + t.getMessage());
+            }
+        });
+    }
+}
