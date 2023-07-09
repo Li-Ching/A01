023b718882966760134366069296f9daf5df4b2b
Li-Ching <c13575@gmail.com>
2023-07-09 15:41:00 +0800

    將sofa_1的家具資料呼叫API使用get方法取得

--- ARHomeDesign/app/src/main/java/com/example/arhomedesign/Sofa_1Fragment.java
+++ ARHomeDesign/app/src/main/java/com/example/arhomedesign/Sofa_1Fragment.java
@@ -1,6 +1,7 @@
 package com.example.arhomedesign;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,38 +14,29 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import com.example.arhomedesign.Methods;
-import com.example.arhomedesign.R;
-import com.example.arhomedesign.RetrofitClient;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class Sofa_1Fragment extends Fragment {
-    private TextView tvFurId, tvType, tvColor, tvStyle, tvBrand, tvPhoneNumber, tvAddress;
+    private TextView tvType, tvColor, tvStyle, tvBrand, tvPhoneNumber, tvAddress;
 
     LinearLayout back;
 
-
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sofa_1, container, false);
-
         back = view.findViewById(R.id.back);
-
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fragmentManager = getParentFragmentManager();
                 fragmentManager.popBackStack();
-
             }
         });
 
-        tvFurId = view.findViewById(R.id.tvFurId);
         tvType = view.findViewById(R.id.tvType);
         tvColor = view.findViewById(R.id.tvColor);
         tvStyle = view.findViewById(R.id.tvStyle);
@@ -53,34 +45,34 @@ public class Sofa_1Fragment extends Fragment {
         tvAddress = view.findViewById(R.id.tvAddress);
 
 
-        super.onViewCreated(view, savedInstanceState);
-
         Methods methods = RetrofitClient.getRetrofitInstance().create(Methods.class);
         Call<furnitures> call = methods.getFurniture();
         call.enqueue(new Callback<furnitures>() {
             @Override
             public void onResponse(Call<furnitures> call, Response<furnitures> response) {
-                if (response.isSuccessful()) {
-                    tvFurId.setText(response.body().getFurnitureId());
-                    tvType.setText(response.body().getType());
-                    tvColor.setText(response.body().getColor());
-                    tvStyle.setText(response.body().getStyle());
-                    tvBrand.setText(response.body().getBrand1());
-                    tvPhoneNumber.setText(response.body().getPhoneNumber());
-                    tvAddress.setText(response.body().getAddress());
+                furnitures furniture= response.body();
+                if (response.isSuccessful() && furniture != null) {
+                    Log.d("API Response", "Type: " + furniture.getType());
+                    Log.d("API Response", "Color: " + furniture.getColor());
+                    Log.d("API Response", "getBrand1: " + furniture.getBrand1());
+                    tvType.setText(furniture.getType());
+                    tvColor.setText(furniture.getColor());
+                    tvStyle.setText(furniture.getStyle());
+                    tvBrand.setText(furniture.getBrand1());
+                    tvPhoneNumber.setText(furniture.getPhoneNumber());
+                    tvAddress.setText(furniture.getAddress());
                 } else {
                     Toast.makeText(getContext(), "An error has occurred", Toast.LENGTH_LONG).show();
+                    Log.e("API Response", "Error: " + response.message());
                 }
             }
 
             @Override
             public void onFailure(Call<furnitures> call, Throwable t) {
-                Toast.makeText(getContext(), "An error has occurred", Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), "onFailure: " + t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
 
-
         return view;
     }
-
 }
