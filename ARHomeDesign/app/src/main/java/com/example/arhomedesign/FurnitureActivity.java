a06c4b130086e4ca9947c5cd982a7909b3fc8b0a
A9233825 <a9233825@ulive.pccu.edu.tw>
2023-08-15 21:52:53 +0700

    設計

--- ARHomeDesign/app/src/main/java/com/example/arhomedesign/FurnitureActivity.java
+++ ARHomeDesign/app/src/main/java/com/example/arhomedesign/FurnitureActivity.java
@@ -2,14 +2,29 @@ package com.example.arhomedesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class FurnitureActivity extends AppCompatActivity {
+    ImageButton back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_furniture);
-
         getSupportActionBar().hide();
+
+        back = findViewById(R.id.backButton);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FurnitureActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- ARHomeDesign/app/src/main/java/com/example/arhomedesign/HomeFragment.java
+++ ARHomeDesign/app/src/main/java/com/example/arhomedesign/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.arhomedesign;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,14 +11,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class HomeFragment extends Fragment {
     ImageButton table, chair, sofa, bed;
+    LinearLayout recommend1, recommend2, recommend3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recommend1 = view.findViewById(R.id.butRec1);
+        recommend2 = view.findViewById(R.id.butRec2);
+        recommend3 = view.findViewById(R.id.butRec3);
 
         table = view.findViewById(R.id.btnTable);
         chair = view.findViewById(R.id.btnChair);
@@ -31,6 +38,30 @@ public class HomeFragment extends Fragment {
         transaction.replace(R.id.HomeFrame, tableFragment);
         transaction.addToBackStack(null);
         transaction.commit();
+
+        recommend1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FurnitureActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        recommend2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FurnitureActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        recommend3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FurnitureActivity.class);
+                startActivity(intent);
+            }
+        });
 
         table.setOnClickListener(new View.OnClickListener() {
             @Override
